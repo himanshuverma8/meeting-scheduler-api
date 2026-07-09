2e63061fc2745709badebd36977e4dd171e7dcf0
Himanshu <hvinprimary@gmail.com>
2026-07-09 19:09:10 +0530

    feat: add JWT secret to environment validation

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 const envSchema = z.object({
     DATABASE_URL: z.string(),
     PORT: z.coerce.number().default(3000),
+    JWT_SECRET_KEY: z.string()
 })
 
 export type Env = z.infer<typeof envSchema >;
