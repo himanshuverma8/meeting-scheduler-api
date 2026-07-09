21a0ab9eb96320cbd654e9f9822765aa7809261c
Himanshu <hvinprimary@gmail.com>
2026-07-09 19:09:23 +0530

    refactor: move Drizzle client from config into db module

--- src/config/db.ts
+++
@@ -1,8 +0,0 @@
-import * as schema from '../db/schema.js';
-import { env } from './env.js';
-import { Pool } from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-const pool = new Pool({ connectionString: env.DATABASE_URL });
-pool.on('error', (err) => console.error('DB pool error:', err.message));
-export const db = drizzle(pool, { schema });
---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+import * as schema from '../db/schema.js';
+import { env } from '../config/env.js';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+const pool = new Pool({ connectionString: env.DATABASE_URL });
+pool.on('error', (err) => console.error('DB pool error:', err.message));
+export const db = drizzle(pool, { schema });
