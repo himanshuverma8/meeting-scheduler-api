35303d5d1ef8acf42c2390a3982f280b095752d2
Himanshu <hvinprimary@gmail.com>
2026-07-21 15:25:39 +0530

    feat: mount public POST /bookings endpoint in app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import authRouter from "./modules/auth/auth.routes";
 import schedulesRouter from "./modules/schedules/schedules.routes"
 import eventTypesRouter from "./modules/event-types/event-types.routes";
 import availabilityRouter from "./modules/availability/availability.routes";
+import bookingsRouter from "./modules/bookings/bookings.routes";
 import { errorHandler } from "./middleware/error.middleware";
 
 
@@ -18,6 +19,7 @@ app.use('/auth', authRouter);
 app.use('/schedules', schedulesRouter);
 app.use('/event-types', eventTypesRouter);
 app.use('/availability', availabilityRouter);
+app.use('/bookings', bookingsRouter);
 app.use(errorHandler);
 
 
