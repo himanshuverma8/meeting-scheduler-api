73a8e50f92e0b4b3135d5e4bd1235df8e1facf65
Himanshu <hvinprimary@gmail.com>
2026-07-24 17:35:27 +0530

    feat: add delete /event-types/:id route

--- src/modules/event-types/event-types.routes.ts
+++ src/modules/event-types/event-types.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createEventTypeHandler, getEventTypesByIdHandler, getEventTypesHandler, updateEventTypeHandler } from "./event-types.controller";
+import { createEventTypeHandler, deleteEventTypeHandler, getEventTypesByIdHandler, getEventTypesHandler, updateEventTypeHandler } from "./event-types.controller";
 import { authenticationMiddleware } from "../../middleware/auth.middleware";
 
 const router = express.Router();
@@ -13,5 +13,7 @@ router.get('/', getEventTypesHandler);
 router.get('/:id', getEventTypesByIdHandler);
 
 router.patch('/:id', updateEventTypeHandler);
+
+router.delete('/:id', deleteEventTypeHandler);
 
 export default router;
